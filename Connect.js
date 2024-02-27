1d70a511e8e0a6513db1ed0c5b4f588ae0ebc64a
Indranil Bhowmick <146933048+IndranilBhowmick2000@users.noreply.github.com>
2024-02-27 12:07:54 +0530

    Update Connect.js

--- Connect.js
+++ Connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 //connection url
-const connection_url = "mongodb+srv://indranilbhowmick2:<ganger100>@cluster0.cnrhljt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const connection_url = "mongodb+srv://indranilbhowmick2:<password>@cluster0.cnrhljt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 mongoose.set('strictQuery',true)
 mongoose.connect(connection_url).then(()=>console.log("database is connected"))
